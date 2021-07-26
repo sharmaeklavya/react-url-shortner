dd004139f6ce5ff32543c0a9ad88f6f6bbb4fb28
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-27 02:36:42 +0530

    bug fixed

--- src/components/sub-components/Redirect.js
+++ src/components/sub-components/Redirect.js
@@ -2,17 +2,14 @@ import { useEffect } from "react";
 import Axios from "axios";
 
 function Redirect() {
-  useEffect(() => {
-    const code = window.location.pathname;
-    Axios.get(`https://node-mini.herokuapp.com${code}`)
-      .then((res) => redirect(res.data[0].shorturl))
-      .catch((err) => console.log(err.response));
+  const code = window.location.pathname;
+  Axios.get(`https://node-mini.herokuapp.com${code}`)
+    .then((res) => redirect(res.data[0].shorturl))
+    .catch((err) => console.log(err.response));
 
-    const redirect = (url) => {
-      window.location.href = url;
-    };
-    return () => console.log("successful");
-  }, []);
+  const redirect = (url) => {
+    window.location.href = url;
+  };
 
   return null;
 }
